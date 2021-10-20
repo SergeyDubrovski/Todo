9526cd398cea0f977d7a359ac7d159938b9a0892
SergeyDubrovski <sovelectron@gmail.com>
2021-10-20 06:15:13 +0300

    first commit7

--- client/src/components/start/ToDo.jsx
+++ client/src/components/start/ToDo.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import s from './ToDo.module.css'; 
  
-const ToDo = () => {
+const ToDo = (props) => {
   return (
     <div>
       <div className={s.box}>
         <h1>Тренировка дня</h1>
         <form className={s.form} type="submit">
-          <input className={s.text} value="" autofocus></input>
+          <input className={s.text} value="" autofocus />
         </form>
         <div className={s.listbox}></div>
         <button className={s.button}>Удалить</button>
